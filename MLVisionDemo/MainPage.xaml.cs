e73d176a0fc9a55491af34a82b54c87545be8079
Kelsey Reid <zenoboe@gmail.com>
2019-10-04 23:54:37 -0500

    more updates from tutorial

--- MLVisionDemo/MainPage.xaml.cs
+++ MLVisionDemo/MainPage.xaml.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using System.Threading.Tasks;
 using TheSeeingPi;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media;
 using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
 using Windows.Storage;
 using Windows.System.Display;
+using Windows.System.Threading;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -28,11 +34,19 @@ namespace MLVisionDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private readonly SemaphoreSlim _frameProcessingSemaphore = new SemaphoreSlim(1);
+
+        private ThreadPoolTimer _frameProcessingTimer;
+
+        public VideoEncodingProperties VideoProperties;
+
         public MainPage()
         {
             this.InitializeComponent();
             StartVideoPreviewAsync();
             LoadModelAsync();
+
         }
 
 
@@ -47,6 +61,10 @@ namespace MLVisionDemo
 
             PreviewControl.Source = _mediaCapture;
             await _mediaCapture.StartPreviewAsync();
+
+            TimeSpan timerInterval = TimeSpan.FromMilliseconds(66); //15fps
+            _frameProcessingTimer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(ProcessCurrentVideoFrame), timerInterval);
+            VideoProperties = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview) as VideoEncodingProperties;
         }
 
         private string _modelFileName = "mycustomvision.onnx";
@@ -63,5 +81,33 @@ namespace MLVisionDemo
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => StatusText.Text = $"Loaded {_modelFileName}");
         }
 
+
+        private async void ProcessCurrentVideoFrame(ThreadPoolTimer timer)
+        {
+            if (_mediaCapture.CameraStreamState != Windows.Media.Devices.CameraStreamState.Streaming || !_frameProcessingSemaphore.Wait(0))
+            {
+                return;
+            }
+
+            try
+            {
+                using (VideoFrame previewFrame = new VideoFrame(BitmapPixelFormat.Bgra8, (int)VideoProperties.Width, (int)VideoProperties.Height))
+                {
+                    await _mediaCapture.GetPreviewFrameAsync(previewFrame);
+
+                    // Evaluate the image
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => StatusText.Text = $"Analyzing frame {DateTime.Now.ToLongTimeString()}");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception with ProcessCurrentVideoFrame: " + ex);
+            }
+            finally
+            {
+                _frameProcessingSemaphore.Release();
+            }
+        }
     }
 }
